3f05c834b539404405514e14df599aa7a07e9605
ESCUDIA <escudia.lifestyle@gmail.com>
2025-09-04 13:56:19 -0700

    SMX github page

--- js/components.js
+++ js/components.js
@@ -1,11 +1,11 @@
-export {default as productRow} from "components/product-row.js";
-export {default as loginForm} from "components/login-form.js";
-export {default as selectMenu} from "components/select-menu.js";
-export {default as orderSummary} from "components/order-summary.js";
-export {default as trackingRow} from "components/tracking-row.js";
-export {default as xlsxParser} from "components/xlsx-parser.js";
-export {default as accountCard} from "components/account-card.js";
-export {default as shippingForm} from "components/shipping-form.js";
+export {default as productRow} from "./components/product-row.js";
+export {default as loginForm} from "./components/login-form.js";
+export {default as selectMenu} from "./components/select-menu.js";
+export {default as orderSummary} from "./components/order-summary.js";
+export {default as trackingRow} from "./components/tracking-row.js";
+export {default as xlsxParser} from "./components/xlsx-parser.js";
+export {default as accountCard} from "./components/account-card.js";
+export {default as shippingForm} from "./components/shipping-form.js";
 
 
 export async function init() {
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import * as components from "components.js";
+import * as components from "/components.js";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
